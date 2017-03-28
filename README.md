aea8944add888d9879366177450077c84dd70554
Nathan Reid <nathan-code@nathantreid.com>
2017-03-28 07:32:04 -0500

    Support vue-class-component syntax

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Integrate TypeScript with vue single-file components for Meteor
 
 This meteor package adds [TypeScript](https://www.typescriptlang.org/) support in your single-file `.vue` components.
-[TypeScript class component's](https://vuejs.org/v2/guide/typescript.html) are not currently supported.
 
 ## Prerequisites
 This package is an add-on for [akryum:vue-component](https://github.com/Akryum/vue-meteor). You must first have that package installed.
@@ -26,7 +25,23 @@ export default {
 </script>
 ```
 
----
+Or with `vue-class-component`:
+```html
+<script lang="typescript">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  let message: string;
+
+  @Component
+  export default class MyButton extends Vue {
+    mounted() {
+      message = 'world';
+      console.log(`Hello, ${message}!`);
+    }
+  }
+</script>
+```
 
 ## Importing TypeScript files
 This plugin only handles compilation of TypeScript within .vue files. To import other TypeScript files, you must install a .ts compiler such as [barbatus:typescript](https://github.com/barbatus/typescript).
--- vue-typescript.js
+++ vue-typescript.js
@@ -6,14 +6,11 @@ import PartialInputFile from './partial-input-file';
 
 global.vue = global.vue || {}
 global.vue.lang = global.vue.lang || {}
-const typescriptCompiler = new TypeScriptCompiler();
+const typescriptCompiler = new TypeScriptCompiler({ module: 'ES6' });
 global.vue.lang.typescript = Meteor.wrapAsync(typescriptHandler);
 
 function typescriptHandler({ source, inputFile }, cb) {
   try {
-    // source = source.replace(/export\s+default/g, 'return');
-    const exportPlaceholder = 'var ___vue_placeholder___ =';
-    source = source.replace(/export\s+default/g, exportPlaceholder);
     // console.log('pre process\n', source);
     const partialInputFile = new PartialInputFile({ contents: source }, inputFile);
     const filesToProcess = [partialInputFile];
@@ -21,20 +18,18 @@ function typescriptHandler({ source, inputFile }, cb) {
     if (fs.existsSync(Plugin.convertToOSPath(tsConfigPath))) {
       const tsConfigFile = new PartialInputFile({
         contents: Plugin.fs.readFileSync(tsConfigPath),
-        filePath: 'tsconfig.json'
+        filePath: 'tsconfig.json',
       }, inputFile);
       filesToProcess.push(tsConfigFile);
     }
 
     typescriptCompiler.processFilesForTarget(filesToProcess);
     const result = Promise.await(partialInputFile.processingPromise);
-    result.data = result.data.replace(new RegExp(exportPlaceholder, 'g'), 'export default');
-
     // console.log('data\n', result.data);
     cb(null, {
       script: result.data,
       map: result.sourceMap,
-      useBabel: false
+      useBabel: true
     });
   } catch (err) {
     cb(err);
